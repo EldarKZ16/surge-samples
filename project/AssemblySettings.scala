54f6500bc5c0d3729ebe835706b9fc27a2ba4f9c
Jeff Boutotte <jeff.boutotte@ukg.com>
2021-12-20 15:58:24 -0500

    Fix assembly

--- project/AssemblySettings.scala
+++ project/AssemblySettings.scala
@@ -3,13 +3,16 @@
 import sbt.{ AutoPlugin, Def, PluginTrigger }
 import sbtassembly.AssemblyKeys._
 import sbtassembly.AssemblyPlugin.autoImport.{ assembly, MergeStrategy }
+import sbtassembly.PathList
 
 object AssemblySettings extends AutoPlugin {
   override def trigger: PluginTrigger = noTrigger
 
   override def projectSettings: Seq[Def.Setting[_]] = Seq(assemblyMergeStrategy := {
     // Discard module-info.class from jackson.  See https://github.com/sbt/sbt-assembly/issues/391
-    case "module-info.class" => MergeStrategy.discard
+    case "module-info.class"                                        => MergeStrategy.discard
+    case PathList("META-INF", "versions", "9", "module-info.class") => MergeStrategy.discard
+    case "version.conf"                                             => MergeStrategy.concat
     case x =>
       val oldStrategy = (assembly / assemblyMergeStrategy).value
       oldStrategy(x)
